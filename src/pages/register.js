de538b4791b01e1195153da5e789328d642af0e4
Vrinda6904 <vrindamathur2004@gmail.com>
2026-02-25 22:23:23 +0530

    url change

--- src/pages/register.js
+++ src/pages/register.js
@@ -11,7 +11,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch('http://localhost:5000/register', {
+      const res = await fetch('https://budgetbuddy-backend-13xd.onrender.com/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name, email, password }),
